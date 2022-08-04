4a223e87acdef6ea0827d3e224733c0011d6d7b0
godManuel <emmanuelabraham375@gmail.com>
2022-08-04 17:10:49 +0100

    Added Header & Button components

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,39 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="container">
+     <Header title="Task Tracker" />
+  </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<template>
+  <button>Add Task</button>
+</template>
+
+<script>
+export default {
+  name: 'Button'
+}
+
+</script>
